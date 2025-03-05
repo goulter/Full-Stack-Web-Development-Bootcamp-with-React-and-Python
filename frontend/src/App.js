ee9c001fb020aeb2ae29ba77a6440e39e66f1ba1
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-03-05 08:30:13 -0800

    created initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+
+const App = () => {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
