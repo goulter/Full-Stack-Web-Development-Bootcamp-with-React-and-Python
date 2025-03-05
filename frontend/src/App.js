659590dcbbdf8b71283706f35fce40c5947cc7f7
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-03-05 14:44:14 -0800

    create search component and add it to the app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
+import Search from './components/search';
 
 const App = () => {
   return (
     <div className="App">
       <Header title="Images Gallery"/>
+      <Search />
     </div>
   );
 }
