19dedba65c2de8b7a60c1811eb60a9f9d40e9a8a
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-03-24 10:00:22 -0700

    implement state, set unsplash key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/search';
+import { useState } from 'react';
 
-const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log(e.target[0].value);
-}
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+console.log(UNSPLASH_KEY);
 
 const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+  console.log(process.env)
+
   return (
     <div className="App">
       <Header title="Images Gallery"/>
-      <Search handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
