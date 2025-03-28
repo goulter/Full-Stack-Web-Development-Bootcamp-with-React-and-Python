0ffe518d3b426e7873582008025804844f493dd3
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-03-28 10:19:52 -0700

    connect to the unsplash api and read the response

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,9 +12,20 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+
+    //the following api url is derived from
+    //https://unsplash.com/documentation#location, https://unsplash.com/documentation#public-authentication, https://unsplash.com/documentation#get-a-random-photo
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`) //returns a promise that resolves to a Response object
+      .then((res) => res.json()) //returns a promise that resolves to the parsed JSON data
+      .then((unsplash_data) => { //handles the parsed JSON data
+        console.log(unsplash_data);
+      })
+      .catch((err) => { //handles and error from the fetch request or JSON parsing.
+        console.log(err);
+      })
   }
 
-  console.log(process.env)
+ // console.log(process.env)
 
   return (
     <div className="App">
