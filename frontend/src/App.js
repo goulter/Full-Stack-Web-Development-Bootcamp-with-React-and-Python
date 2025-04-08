256408686ea0673b45d14d0ee599c816baf60fa0
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-04-08 08:50:12 -0700

    save queried search images to images array, log the array when the state of the app component changes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,17 @@ import Search from './components/search';
 import { useState } from 'react';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-console.log(UNSPLASH_KEY);
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images); 
+  /*
+  call it here instead and it will log every time the page (re)renders 
+  and the state of the images array will have the updated value.
+  it was also log when the state of the app component changes when we type the query with setWord onChange
+  */ 
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +25,8 @@ const App = () => {
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`) //returns a promise that resolves to a Response object
       .then((res) => res.json()) //returns a promise that resolves to the parsed JSON data
       .then((unsplash_data) => { //handles the parsed JSON data
-        console.log(unsplash_data);
+        setImages([unsplash_data, ...images]) //update the existing images array with the new image as well as future search images. New at the front 
+        // was here --> console.log(images);
       })
       .catch((err) => { //handles and error from the fetch request or JSON parsing.
         console.log(err);
