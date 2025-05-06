d7dd92b018bb9dab487f305f0a8121ef754a1af1
Glen Goulter <37945000+goulter@users.noreply.github.com>
2025-05-06 09:06:09 -0700

    show the welcome component if there are no images in the array

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Search from './components/search';
 import ImageCard from './components/imagecard';
 import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Welcome from './components/welcome';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -45,15 +46,17 @@ const App = () => {
       <Header title="Images Gallery"/>
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
       <Container className="mt-4">
-        <Row xs={1} md={2} lg={3}>
-          {images.map((image,index) => 
-            (
-              <Col key={index} className='pb-3'>
-                <ImageCard image={image} deleteImage={handleDelete}/>
-              </Col>
-            )
-          )}
-        </Row>
+        {images.length ? 
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image,index) => 
+              (
+                <Col key={index} className='pb-3'>
+                  <ImageCard image={image} deleteImage={handleDelete}/>
+                </Col>
+              )
+            )}
+          </Row>:
+          <Welcome/>}
       </Container>
     </div>
   );
